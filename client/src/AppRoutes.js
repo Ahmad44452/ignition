d9bbb7f8d9f01c8b896ee56212759da71b271a19
Ahmad44452 <ahmadghani44452@gmail.com>
2022-08-25 11:16:49 +0500

    responsiveness added

--- client/src/AppRoutes.js
+++ client/src/AppRoutes.js
@@ -2,25 +2,23 @@
 import "./styles/mainStyles.scss";
 /////////////////////////////////////////
 import React from "react";
-import { unstable_HistoryRouter as HistoryRouter, Routes, Route } from "react-router-dom";
-import { createBrowserHistory } from "history";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./components/Home";
 import Login from "./components/Login";
 
 import MainLayout from "./hoc/MainLayout";
 
-export const historyObject = createBrowserHistory({ window });
 
 const AppRoutes = () => {
   return (
     <MainLayout>
-      <HistoryRouter history={historyObject}>
+      <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/" element={<Home />} />
         </Routes>
-      </HistoryRouter>
+      </BrowserRouter>
     </MainLayout>
   )
 }
--- client/src/components/Home/homeContact.js
+++ client/src/components/Home/homeContact.js
@@ -4,23 +4,25 @@ const HomeContact = () => {
   return (
     <div className="homeContact">
       <h2 className="homeContact__heading">Contact Us</h2>
+      <div className="homeContact__form--container">
+        <form className="homeContact__form">
+          <div className="homeContact__form--group">
+            <input autoComplete="off" type="text" className="homeContact__form--input" placeholder="Full name" id="homeContact__name" />
+            <label htmlFor="homeContact__name" className="homeContact__form--label">Full name</label>
+          </div>
+          <div className="homeContact__form--group">
+            <input autoComplete="off" type="email" className="homeContact__form--input" placeholder="Email address" id="homeContact__email" />
+            <label htmlFor="homeContact__email" className="homeContact__form--label">Email address</label>
+          </div>
+          <div className="homeContact__form--group">
+            <textarea autoComplete="off" rows={7} type="text" className="homeContact__form--input homeContact__form--textarea" placeholder="Message" id="homeContact__message" />
+          </div>
+          <div className="homeContact__form--group">
+            <button onClick={(e) => e.preventDefault()} className="homeContact__form--submit" type="submit">Send</button>
+          </div>
+        </form>
+      </div>
 
-      <form className="homeContact__form">
-        <div className="homeContact__form--group">
-          <input autoComplete="off" type="text" className="homeContact__form--input" placeholder="Full name" id="homeContact__name" />
-          <label htmlFor="homeContact__name" className="homeContact__form--label">Full name</label>
-        </div>
-        <div className="homeContact__form--group">
-          <input autoComplete="off" type="email" className="homeContact__form--input" placeholder="Email address" id="homeContact__email" />
-          <label htmlFor="homeContact__email" className="homeContact__form--label">Email address</label>
-        </div>
-        <div className="homeContact__form--group">
-          <textarea autoComplete="off" rows={7} type="text" className="homeContact__form--input homeContact__form--textarea" placeholder="Message" id="homeContact__message" />
-        </div>
-        <div className="homeContact__form--group">
-          <button onClick={(e) => e.preventDefault()} className="homeContact__form--submit" type="submit">Send</button>
-        </div>
-      </form>
 
     </div>
   )
